2cb8d0dba583f75a96c6a187d7d45edf1ffb770b
Robson <robsonribeirorra@gmail.com>
2023-08-02 17:15:28 -0300

    postForm and showPost features

--- server/src/routes/postRoutes.js
+++ server/src/routes/postRoutes.js
@@ -1,13 +1,12 @@
 import { Router } from 'express';
 const router = new Router();
 
-import {createPost, updatePost, searchPost, test} from '../controller/postController.js';
+import { createPost, updatePost, searchPost, newPost, showPost } from '../controller/postController.js';
 
+router.get('/show/:id', showPost);
+router.get('/new', newPost);
 router.post('/create', createPost);
 router.post('/update', updatePost);
 router.post('/search', searchPost);
-
-router.get('/test', test);
-
 
 export default router;
